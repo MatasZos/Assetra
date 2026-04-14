027aa2e75f4e5b1c91bd9b43b43d7acdfed13f68
MatasZos <B00163194@mytudublin.ie>
2026-04-15 00:12:42 +0100

    add home view and create home.html template for the inventory app

--- inventory/views.py
+++ inventory/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 # Create your views here.
 
+#def home(request):
+ #   return HttpResponse("Welcome to the School Inventory System")
+#
+
+from django.shortcuts import render
+
 def home(request):
-    return HttpResponse("Welcome to the School Inventory System")
+    return render(request, 'inventory/home.html')
