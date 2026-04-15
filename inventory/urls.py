a7683f5cf8a43b40e752433e5f20797d1447c369
MatasZos <B00163194@mytudublin.ie>
2026-04-15 18:57:56 +0100

    logout redirect

--- inventory/urls.py
+++ inventory/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('manage_requests/', views.manage_requests, name='manage_requests'),
     path('approve/<int:request_id>/', views.approve_request, name='approve_request'),
     path('reject/<int:request_id>/', views.reject_request, name='reject_request'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
