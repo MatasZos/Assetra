22e553fbf04db42ab04bff2b3ce27f50a567805a
MatasZos <B00163194@mytudublin.ie>
2026-04-15 15:34:57 +0100

    fix import

--- inventory/views.py
+++ inventory/views.py
@@ -9,7 +9,7 @@
 from django.shortcuts import render
 from .models import Item
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.decorators import staff_member_required    
+from django.contrib.admin.views.decorators import staff_member_required   
 
 from django.shortcuts import redirect
 from .models import Request
