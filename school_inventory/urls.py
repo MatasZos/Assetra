befbc0419db9c2850a952ef4f71936b9bc3390d7
MatasZos <B00163194@mytudublin.ie>
2026-04-15 13:59:40 +0100

    fixing login redirect

--- school_inventory/urls.py
+++ school_inventory/urls.py
@@ -5,6 +5,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('inventory.urls')),
-    path('login/', auth_views.LoginView.as_view(), template_name='inventory/login.html', name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='inventory/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
