398654f46d4c56a51f0b46b21f24cfd67f8d760b
MatasZos <B00163194@mytudublin.ie>
2026-04-15 01:04:04 +0100

    add Item model with name, description, quantity, and category fields; register in admin

--- inventory/models.py
+++ inventory/models.py
@@ -8,3 +8,15 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    quantity = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+    
