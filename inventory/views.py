e56aa03390ace6e6427c3a72d0a515f00cc47bf5
MatasZos <B00163194@mytudublin.ie>
2026-04-23 16:23:19 +0100

    fix by importing user

--- inventory/views.py
+++ inventory/views.py
@@ -2,6 +2,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+
+from django.contrib.auth.models import User
  
 from .models import Item, Category, Request
 from .forms import ItemForm, CategoryForm
