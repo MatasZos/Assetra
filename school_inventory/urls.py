29cf9fd9ec2452f32d552225847c8da30aba86b4
MatasZos <B00163194@mytudublin.ie>
2026-04-25 02:38:58 +0100

    attempt to fix django auth css issue

--- school_inventory/urls.py
+++ school_inventory/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +11,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='inventory/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
